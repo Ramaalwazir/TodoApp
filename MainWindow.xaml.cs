4982cee742d5373fadbfc951db4e4fa1662a556e
ramaalwazir <ramaalwazir6@gmail.com>
2025-01-15 00:12:51 +0100

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,17 +16,52 @@ namespace TodoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private todolist _todolist;
         public MainWindow()
         {
             InitializeComponent();
+            _todolist = new todolist();
+
         }
+        /// <summary>
+        /// lägg till en todo-item i listan.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddButton_Click(object sender, RoutedEventArgs e )
+         
+        {
+            string task =TaskTextBox.Text;
+
+            if (!string.IsNullOrEmpty(task))
+
+            {
+                _todolist.AddTask(task);
+                updateTaskList();
+                TaskTextBox.Clear();
+            }
+        }
+        /// <summary>
+        /// Uppdatera Listan med todo_items.
+        /// </summary>
+        private void updateTaskList()
+        {
+            TasksListBox.Items.Clear();
+            foreach (var task in _todolist.GetAllTask())
+         {
+                TasksListBox.Items.Add(task);
+         }
+       }
+private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
 
+    if(TasksListBox.SelectedIndex >=0 )
+            {
+                _todolist.RemoveTask(TasksListBox.SelectedIndex);
+                updateTaskList();
+           }
         }
-        private void RemoveButton_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
     }
 }
+
