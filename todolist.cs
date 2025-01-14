053d35d7e355ea0ceb1ccee886cc468babd81bf9
ramaalwazir <ramaalwazir6@gmail.com>
2025-01-14 18:39:09 +0100

    class TodoList- logiken

---
+++ todolist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Hantera logiken för att lägga till, ta bort och visa todo-items.
+    /// </summary>
+    public class todolist
+    {
+private List <string> tasks = new List <string> ();
+
+    public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+        public void RemoveTask(int index) 
+        {
+            if (index >= 0 && index < tasks.Count)
+            
+
+            tasks.RemoveAt(index);
+        }
+        
+       public List <string> GetAllTask ()
+        {
+         return tasks;
+        }
+    }
